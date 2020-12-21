63c0595e4c4107890126e5caffd5a3e1cb3c911b
vcow <yvi@ambidexter.io>
2020-12-21 21:09:50 +0300

    Base project with the base editor scene

---
+++ Assets/Scripts/GameInstaller.cs
@@ -0,0 +1,8 @@
+using Zenject;
+
+public class GameInstaller : MonoInstaller<GameInstaller>
+{
+	public override void InstallBindings()
+	{
+	}
+}
---
+++ Assets/Scripts/LevelEditor/UI/UiSelectorController.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+namespace LevelEditor.UI
+{
+	[DisallowMultipleComponent]
+	public class UiSelectorController : MonoBehaviour
+	{
+#pragma warning disable 649
+		[SerializeField] private Canvas _surfacesUi;
+		[SerializeField] private Canvas _trapsUi;
+		[SerializeField] private Canvas _staffUi;
+#pragma warning restore 649
+
+		public void OnSelectSurfaces(bool isOn)
+		{
+			if (!isOn) return;
+			
+			_surfacesUi.gameObject.SetActive(true);
+			_trapsUi.gameObject.SetActive(false);
+			_staffUi.gameObject.SetActive(false);
+		}
+
+		public void OnSelectTraps(bool isOn)
+		{
+			if (!isOn) return;
+			
+			_surfacesUi.gameObject.SetActive(false);
+			_trapsUi.gameObject.SetActive(true);
+			_staffUi.gameObject.SetActive(false);
+		}
+
+		public void OnSelectStaff(bool isOn)
+		{
+			if (!isOn) return;
+			
+			_surfacesUi.gameObject.SetActive(false);
+			_trapsUi.gameObject.SetActive(false);
+			_staffUi.gameObject.SetActive(true);
+		}
+	}
+}
