7dc0f5d40cd24056dae53e47ca88a0386a522d76
Yacow Vircowskiy <vcow@yandex.ru>
2021-01-07 20:16:42 +0300

    Bounds settings

---
+++ Assets/Scripts/Bounds/BoundsSettings.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+	public class BoundsSettings : MonoBehaviour
+	{
+		[SerializeField] private bool _hasOuterBounds = true;
+		[SerializeField] private Rect _innerBounds;
+		[SerializeField] private Rect _outerBounds;
+
+		public bool HasOuterBounds => _hasOuterBounds;
+
+		public Rect InnerBounds => _innerBounds;
+
+		public Rect OuterBounds => _outerBounds;
+	}
+}
